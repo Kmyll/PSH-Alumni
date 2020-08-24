343dc376b428f94bb2e5f88cce273af31c840a4d
kmyll <camille.desoubrie@gmail.com>
2020-08-24 16:08:01 +0200

    [ADD] - resume form

--- src/components/Firestore/index.js
+++ src/components/Firestore/index.js
@@ -2,14 +2,12 @@ import firebase from 'firebase';
 import 'firebase/storage';
 
 const config = {
-	apiKey            : 'AIzaSyCZDjLFkcFbv-_U0NACLmB8inEudquqZa4',
-	authDomain        : 'psh-alumni.firebaseapp.com',
-	databaseURL       : 'https://psh-alumni.firebaseio.com',
-	projectId         : 'psh-alumni',
-	storageBucket     : 'psh-alumni.appspot.com',
-	messagingSenderId : '371602228378',
-	appId             : '1:371602228378:web:18ed44d0f1cbd6b0e44b23',
-	measurementId     : 'G-CJVH3D4YQP'
+	apiKey            : process.env.REACT_APP_API_KEY,
+	authDomain        : process.env.REACT_APP_AUTH_DOMAIN,
+	databaseURL       : process.env.REACT_APP_DATABASE_URL,
+	projectId         : process.env.REACT_APP_PROJECT_ID,
+	storageBucket     : process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId : process.env.REACT_APP_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
--- src/components/screens/offres.js
+++ src/components/screens/offres.js
@@ -8,16 +8,27 @@ import { withAuthorization, withEmailVerification } from '../Session';
 import { UserList, UserItem } from '../Users';
 import * as ROLES from '../../constants/roles';
 import * as ROUTES from '../../constants/routes';
-import Modal from '@material-ui/core/Modal';
+import { render } from 'react-dom';
+import 'react-responsive-modal/styles.css';
+import { Modal } from 'react-responsive-modal';
 
 class offres extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			annonces : [],
-			loading  : false
+			loading  : false,
+			open     : false
 		};
 	}
+
+	onOpenModal = () => {
+		this.setState({ open: true });
+	};
+
+	onCloseModal = () => {
+		this.setState({ open: false });
+	};
 
 	componentDidMount() {
 		const db = firebase.firestore();
@@ -38,15 +49,33 @@ class offres extends Component {
 
 	render() {
 		console.log('this.state', this.state);
-		const { annonces, loading } = this.state;
+		const { annonces, loading, open } = this.state;
 		const list = Object.keys(annonces).map((annonce) => (
-			<div>
+			<div className="offers">
 				<li key={annonces.id}>
 					<div>
 						{annonces[annonce].name} {annonces[annonce].place}
 					</div>
 					<div>{annonces[annonce].contrat}</div>
 					<div>{annonces[annonce].created.seconds}</div>
+					<div>
+						<div className="openJob" onClick={this.onOpenModal}>
+							Voir le poste
+						</div>
+						<Modal open={open} onClose={this.onCloseModal} className="jobAdModale">
+							<h2>{annonces[annonce].name} </h2>
+							<p>
+								<span>Lieu : </span>
+								{annonces[annonce].place}
+								<span>Type de poste : </span>
+								{annonces[annonce].contrat}
+								<span>Description : </span>
+								{annonces[annonce].description}
+								<span>Contact : </span>
+								{annonces[annonce].contact}
+							</p>
+						</Modal>
+					</div>
 				</li>
 			</div>
 		));
